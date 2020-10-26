0e7dd0985dd86b7eb6e11398a339a3232aa702ff
Laurent Capello <laurent.capello@gmail.com>
2020-10-26 14:55:38 +0100

    add prefix before url to request from

--- .history/src/components/MainContent/MainContent_20201026145420.js
+++ .history/src/components/MainContent/MainContent_20201026145420.js
@@ -12,58 +12,37 @@ function MainContent(){
     location: "",
     fullTime: true
   })
-  // const[description, setDescription] = useState("")
-  // const[location, setLocation] = useState("")
-  // const[fullTime, setFullTime] = useState(true)
 
-
-// when the user types a word in either the description or location input field or changes the full time checkbox
-// -change the corresponding value in state
-// -get new data
-// -change the job cards accordingly
-
-//TODO: a function that gets data from the api and save it in data everytime the value of the SearchForm changes
-//TODO: a function that is called whenever the form is changed, it updates the state and the value of the form fields
-// handleChange(event) {
-//   const {name, value, type, checked} = event.target
-//   type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
-// }
-
-  function handleFormChange(e){
+  const handleFormChange = e => {
     const {name, value, type, checked} = e.target
     type === "checkbox"
-    ? setUserInput({...userInput, [name]: checked})
-    : setUserInput({...userInput,[name]: value})
+      ? setUserInput({...userInput, [name]: checked})
+      : setUserInput({...userInput,[name]: value})
   }
 
   const buildUrl = () => {
-    let url = "https://jobs.github.com/positions.json?"
+    let url = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?"
     url += userInput.description ? `&description=${userInput.description}` : ""
     url += userInput.location ? `&location=${userInput.location}` : ""
     url += userInput.fullTime ? `&full_time=${userInput.fullTime}` : ""
-    console.log(`i will query this url: ${url}`)
     return url
     }
 
-  const getData = () => {
-    const url = buildUrl()
+  const getData = url => {
     fetch(url)
     .then(response => response.json())
     .then(data => setData(data))
   }
 
-  const debouncedGetData = (
-    _.debounce(getData, 3000,{leading: true})
-  )
+  const debouncedGetData = useCallback(_.debounce(getData, 1500,{leading: true}),[])
 
   useEffect(() => {
-    debouncedGetData()
-    //getData()
+    const url = buildUrl()
+    debouncedGetData(url)
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userInput])
 
   useEffect(() => {
-    console.log(`data: ${data}`)
     let cards = []
     if(data.length !== 0) {
       cards = data.map(job => {
@@ -71,11 +50,12 @@ function MainContent(){
           key={job.id}
           date={job.created_at}
           logo={job.company_logo}
-          company={job.company_name}
+          company={job.company}
           location={job.location}
           type={job.type}
           title={job.title}
           url={job.url}
+          description={job.description}
         />
      })
     }
@@ -84,7 +64,7 @@ function MainContent(){
   }, [data])
 
     return (
-      <div>
+      <div id="main">
         <SearchForm
           handleFormChange={handleFormChange}
           userInput={userInput}
--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -21,7 +21,7 @@ function MainContent(){
   }
 
   const buildUrl = () => {
-    let url = "https://jobs.github.com/positions.json?"
+    let url = "https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?"
     url += userInput.description ? `&description=${userInput.description}` : ""
     url += userInput.location ? `&location=${userInput.location}` : ""
     url += userInput.fullTime ? `&full_time=${userInput.fullTime}` : ""

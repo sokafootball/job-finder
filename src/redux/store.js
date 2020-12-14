4b75c187e76ed86a3196afcb1c0f4b87f0501ac8
Laurent Capello <laurent.capello@gmail.com>
2020-12-14 18:16:42 +0100

    create store with configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { configureStore } from '@reduxjs/toolkit'
 import dataReducer from './data/dataReducer'
 import userInputReducer from './userInput/userInputReducer'
 
-const thunkMiddleware = require('redux-thunk').default
-
-const rootReducer = combineReducers({
+const rootReducer = {
   userInput: userInputReducer,
   data: dataReducer,
-})
+}
 
-const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware))
-
-const store = createStore(rootReducer, composedEnhancer)
+const store = configureStore({ reducer: rootReducer })
 
 export default store

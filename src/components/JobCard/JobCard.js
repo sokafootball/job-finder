22474c24d689c53a36f2dff9d67edc9cafb69757
Laurent Capello <laurent.capello@gmail.com>
2021-01-02 16:10:59 +0100

    convert jobCard to bem

--- src/components/JobCard/JobCard.js
+++ src/components/JobCard/JobCard.js
@@ -10,26 +10,30 @@ function JobCard(props) {
       : title
 
   return (
-    <div id="card">
-      <div id="card-header">
+    <div className="card">
+      <div className="card__header">
         <a
-          id="job-title"
+          className="card__title"
           href={props.url}
           target="_blank"
           rel="noopener noreferrer"
         >
           {title}
         </a>
-        <p id="date">{props.created_at}</p>
+        <p className="card__date">{props.created_at}</p>
       </div>
-      <div id="details">
-        <span id="location">{props.location}</span>
-        <span id="separator "> - </span>
-        <span id="type">{props.type}</span>
+      <div className="card__details">
+        <span className="card__location">{props.location}</span>
+        <span className="card__separator "> - </span>
+        <span className="card__type">{props.type}</span>
       </div>
-      <div id="company-div">
-        <img id="company-logo" src={props.company_logo} alt="N/A"></img>
-        <p id="company-name">{props.company}</p>
+      <div className="card__company-div">
+        <img
+          className="card__company-logo"
+          src={props.company_logo}
+          alt="N/A"
+        ></img>
+        <p className="card__company-name">{props.company}</p>
       </div>
     </div>
   )

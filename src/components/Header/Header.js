ec06d7190fb8cab444fef0cb46ed304f7e473ba5
Laurent Capello <laurent.capello@gmail.com>
2021-01-02 16:04:23 +0100

    convert title to bem

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import './Header.scss'
 import React from 'react'
 
 function Header() {
-  return <p id="title">Job Finder</p>
+  return <p className="title">Job Finder</p>
 }
 
 export default Header

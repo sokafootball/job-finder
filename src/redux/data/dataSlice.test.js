35d68777340fa62c1f6a6c2bf567c7327b5ef997
Laurent Capello <laurent.capello@gmail.com>
2021-01-21 16:11:41 +0100

    add test for dataSlice reducer for fulfilled and rejected actions

--- src/redux/data/dataSlice.test.js
+++ src/redux/data/dataSlice.test.js
@@ -2,11 +2,28 @@ import { ERROR, LOADED, LOADING } from '../../shared/constants'
 import { dataSliceActions, dataSliceReducer } from './dataSlice'
 
 describe('dataSlice reducers', () => {
+  const initialState = { data: [], loadingStatus: LOADED }
   it('should handle Pending', () => {
-    const initialState = { data: [], loadingStatus: LOADED }
     expect(dataSliceReducer(initialState, dataSliceActions.pending())).toEqual({
       ...initialState,
       loadingStatus: LOADING,
+    })
+  })
+  it('should handle Rejected', () => {
+    expect(dataSliceReducer(initialState, dataSliceActions.rejected())).toEqual(
+      {
+        ...initialState,
+        loadingStatus: ERROR,
+      }
+    )
+  })
+  it('should handle Fulfilled', () => {
+    const payload = 'payload'
+    const action = dataSliceActions.fulfilled(payload)
+    expect(dataSliceReducer(initialState, action)).toEqual({
+      ...initialState,
+      loadingStatus: LOADED,
+      data: action.payload,
     })
   })
 })

76fb710c5bcc8467c376446748e0818dd2979007
Laurent Capello <laurent.capello@gmail.com>
2021-01-22 16:51:08 +0100

    add enzyme dependency, configured test files, created Footer snapshot test

---
+++ src/redux/epics/userInputEpic.test.js
@@ -0,0 +1,11 @@
+// import { updateUserInput } from '../userInput/userInputSlice'
+// import { userInputEpic } from './userInputEpic'
+
+// describe('userInputEpic', () => {
+//   it(`should only handle actions of type ${updateUserInput.toString()}`, () => {
+//     expect(userInputEpic({ type: 'wrong type' }).toEqual())
+//   })
+// })
+describe('userInputEpic', () => {
+  it(`placeholder test`, () => {})
+})

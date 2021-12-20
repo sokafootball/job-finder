4ecbfe40f859b51738b95a2164b5b2adcf7e5168
Laurent Capello <laurent.capello@gmail.com>
2021-12-20 11:43:34 +0100

    add fixed values to some query parameters

--- src/shared/buildUrl.js
+++ src/shared/buildUrl.js
@@ -1,14 +1,22 @@
+import { CLIENT_ID } from '../../shared/credentials'
+
 export const buildUrl = (
-  userId,
   keyword,
-  location,
-  radius,
-  days,
-  sortColumns,
-  sortOrder,
-  startRow,
-  pageSize
+  location
+  // radius,
+  // days,
+  // sortColumns,
+  // sortOrder,
+  // startRow,
+  // pageSize
 ) => {
+  const radius = 20
+  const days = 20
+  const sortColumns = 'accquisitiondate'
+  const sortOrder = 'ASC'
+  const startRow = 1
+  const pageSize = 20
+  const userId = CLIENT_ID
   const herokuAppUrl = 'https://laurent-cors-proxy.herokuapp.com/'
   const apiUrl = `https://api.careeronestop.org/v1/jobsearch/${userId}/${keyword}/${location}/${radius}/${sortColumns}/${sortOrder}/${startRow}/${startRecord}/${pageSize}/${days}`
   const finalUrl = `${herokuAppUrl}${apiUrl}`
